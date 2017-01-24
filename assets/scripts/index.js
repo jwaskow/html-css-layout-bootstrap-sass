2a50c6d29635c23cd2653506c73d139ee26fdd15
jwaskow <thewasko@gmail.com>
2017-01-24 11:47:19 -0500

    Test Deployment

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,8 +3,18 @@
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
 
+
+
 $(() => {
   setAPIOrigin(location, config);
+
+
+  const saveChanges = function (event) {
+    event.preventDefault();
+    let input = $("#name").val();
+    console.log(input);
+  };
+    $('#saveChange').on('save-changes', saveChanges);
 });
 
 // use require with a reference to bundle the file and use it in this file
